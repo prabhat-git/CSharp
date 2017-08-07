097093eebb6f82054cd5bcb4cd62be6d34e6f556
Prabhat Singh <prabhat.email@gmail.com>
2017-08-07 13:37:40 -0500

    Equality first commit.

---
+++ Equality/src/Equality/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Equality
+{
+    public class Program
+    {
+        public struct Person
+        {
+            public string Name { get; set; }
+
+            public Person(string name)
+            {
+                Name = name;
+            }
+
+            public override string ToString()
+            {
+
+                return Name;
+            }
+            public static bool operator ==(Person p1, Person p2)
+            {
+                return Equals(p1, p2);
+            }
+            public static bool operator !=(Person p1, Person p2)
+            {
+                return !Equals(p1, p2);
+            }
+        }
+        public static void Main(string[] args)
+        {
+                Person p1 = new Person("Ehsan Sajjad");
+                Person p2 = new Person("Ehsan Sajjad");
+
+                Console.WriteLine(p1.Equals(p2));
+                Console.WriteLine(p1 == p2);
+                Console.ReadKey();
+        }
+    }
+}
---
+++ Equality/src/Equality/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Equality")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b3e6cacb-de06-4901-a7fd-b2f23046f12e")]
